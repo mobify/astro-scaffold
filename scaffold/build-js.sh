94d4c9155b5c9b7fcd065a436fba2fca58d72b50
Noah Adams <noah.adams@gmail.com>
2015-04-16 18:31:22 -0700

    control gradle's PATH for npm/node using gradle.properties

--- scaffold/build-js.sh
+++ scaffold/build-js.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-# This script is to be executed by Gradle.
-
-# Android studio launches Gradle in a subprocess that doesn't inherrit your
-# environment variables. This blows up when we expect some command like 'grunt'
-# to be available in your PATH. Source all files that typically update the PATH
-# to ensure that Gradle has the same PATH as your usual terminal.
-source ~/.zshrc
-source ~/.bashrc
-source ~/.bash_profile
-
 set -e
 
 # Build app.js.
