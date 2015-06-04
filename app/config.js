5f4927540903cce52305718158e8fdc5fd7f4d0f
Liz Cross <elizabeth@mobify.com>
2015-06-04 13:46:44 -0700

    Updating to remove .address from plugin params. Updated with changes to application structure. ie. it's not a plugin.

--- app/config.js
+++ app/config.js
@@ -1,11 +1,12 @@
 require.config({
     baseUrl: '.',
     paths: {
-    	'astro': '../node_modules/astro-sdk/js/src/astro',
-      'app': '../node_modules/astro-sdk/js/src/app',
+        'astro': '../node_modules/astro-sdk/js/src/astro',
+        'application': '../node_modules/astro-sdk/js/src/application',
+        'worker': '../node_modules/astro-sdk/js/src/worker',
     	'plugin-manager': '../node_modules/astro-sdk/js/src/plugin-manager',
     	'plugins': '../node_modules/astro-sdk/js/src/plugins',
     	'vendor/backbone-events': '../node_modules/astro-sdk/js/vendor/backbone-events',
-      'bluebird': 'node_modules/bluebird/js/browser/bluebird'
+        'bluebird': 'node_modules/bluebird/js/browser/bluebird'
     }
 });
