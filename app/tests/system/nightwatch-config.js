b62286b2b1ecd39b729af27452f1b724ff096a47
Mike Klemarewski <mikek@mobify.com>
2015-07-22 11:36:48 -0700

    Add automated appium tests

---
+++ app/tests/system/nightwatch-config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var appPath = path.join(process.cwd(), '../ios/build/Build/Products/Debug-iphonesimulator/scaffold.app');
+
+module.exports = {
+    "src_folders": ["./tests/system"],
+    "output_folder": "./reports",
+    "custom_commands_path": "./node_modules/nightwatch-commands/commands",
+    "custom_assertions_path": "./node_modules/nightwatch-commands/assertions",
+
+    "selenium": {
+        "start_process": true,
+        "server_path": "./node_modules/nightwatch-commands/selenium/selenium-server.jar",
+        "log_path": "./node_modules/nightwatch-commands/selenium/",
+        "host": "127.0.0.1",
+        "port": 4444,
+        "cli_args": {
+            "webdriver.chrome.driver": "./node_modules/nightwatch-commands/selenium/drivers/chromedriver"
+        }
+    },
+
+    "test_settings": {
+        "default": {
+            "globals" : {
+                "waitForConditionTimeout" : 60000
+            },
+            "launch_url": "http://localhost:4723/wd/hub",
+            "selenium_host": "localhost",
+            "selenium_port": 4723,
+            "silent": true,
+            "output": true,
+            "screenshots": {
+                "enabled": false,
+                "path": ""
+            },
+            "desiredCapabilities": {
+                "app": "com.mobify.builds.RitchieBrothers",
+                "platformName": "iOS",
+                "platformVersion": "8.4",
+                "deviceName": "Dinah",
+                "udid": "2d34635acb263fce25892ecae9c02eacab302eff"
+            }
+        },
+
+        "ios-sim": {
+            "desiredCapabilities": {
+                "fullReset": false,
+                "app": appPath,
+                "platformName": "iOS",
+                "platformVersion": "8.4",
+                "deviceName": "iPhone 6"
+            }
+        }
+    }
+};
--- scripts/ios-appium.sh
+++ scripts/ios-appium.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-pushd ios/scaffold/
+# Kill background processes when this script exits.
+trap 'kill $(jobs -p)' EXIT
+
+appium &
+
+pushd ios/
 xcodebuild \
     -workspace "scaffold.xcworkspace/" \
     -scheme "scaffold" \
     -destination "platform=iOS Simulator,name=iPhone 6,OS=8.4" \
     -derivedDataPath "build" \
-    clean build
+    build
 popd
-
-appium &
-APPIUM_PID=$!
-
-sleep 5
 
 pushd app/
 grunt nightwatch -e ios-sim
-RESULT=$!
-
-kill $APPIUM_PID
-exit $RESULT
