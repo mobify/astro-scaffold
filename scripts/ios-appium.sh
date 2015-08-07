5b252b2e24c99fe1fdbda8124aa6a4cd322b0e60
Ellen Wong <ellen@mobify.com>
2015-08-06 17:48:52 -0700

    clean up xcodebuild output using xcpretty

--- scripts/ios-appium.sh
+++ scripts/ios-appium.sh
@@ -3,15 +3,24 @@
 # Kill background processes when this script exits.
 trap 'kill $(jobs -p)' EXIT
 
+function prettifyOutput() {
+    # xcpretty makes output nicer
+    if which xcpretty 2>/dev/null; then
+        xcpretty -t
+    else
+        cat
+    fi
+}
+
 appium &
 
 pushd ios/
 xcodebuild \
     -workspace "scaffold.xcworkspace/" \
     -scheme "scaffold" \
-    -destination "platform=iOS Simulator,name=iPhone 6,OS=8.4" \
+    -destination "platform=iOS Simulator,name=iPhone 6,OS=8.3" \
     -derivedDataPath "build" \
-    build
+    build | prettifyOutput
 popd
 
 pushd app/
