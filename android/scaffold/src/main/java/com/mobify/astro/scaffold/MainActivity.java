17db429d4eac9d712c69af8df093492caa123003
Loki Meyburg <loki@aristolabs.com>
2015-09-02 20:49:23 -0700

    Bumped Astro submodule to latest. Added new plugins to Android

--- android/scaffold/src/main/java/com/mobify/astro/scaffold/MainActivity.java
+++ android/scaffold/src/main/java/com/mobify/astro/scaffold/MainActivity.java
@@ -3,15 +3,16 @@ package com.mobify.astro.scaffold;
 import android.os.Bundle;
 
 import com.mobify.astro.AstroActivity;
-import com.mobify.astro.plugins.AnimationPlugin;
 import com.mobify.astro.plugins.AnchoredLayoutPlugin;
 import com.mobify.astro.plugins.AstroWorker;
+import com.mobify.astro.plugins.TabBarPlugin;
 import com.mobify.astro.plugins.loaders.DefaultLoaderPlugin;
 import com.mobify.astro.plugins.ImageViewPlugin;
 import com.mobify.astro.plugins.HeaderBarPlugin;
 import com.mobify.astro.plugins.ModalViewPlugin;
-import com.mobify.astro.plugins.webviewplugin.WebViewPlugin;
 import com.mobify.astro.plugins.DrawerPlugin;
+import com.mobify.astro.plugins.NavigationPlugin;
+import com.mobify.astro.plugins.webviewplugin.WebViewPlugin;
 
 import org.apache.cordova.CordovaWebView;
 
@@ -26,12 +27,13 @@ public class MainActivity extends AstroActivity {
         // TODO: In the future we probably want to load this from a configuration file.
         pluginManager.register(DrawerPlugin.class);
         pluginManager.register(WebViewPlugin.class);
+        pluginManager.register(NavigationPlugin.class);
         pluginManager.register(AnchoredLayoutPlugin.class);
         pluginManager.register(ModalViewPlugin.class);
         pluginManager.register(HeaderBarPlugin.class);
-        pluginManager.register(AnimationPlugin.class);
         pluginManager.register(ImageViewPlugin.class);
         pluginManager.register(DefaultLoaderPlugin.class);
+        pluginManager.register(TabBarPlugin.class);
 
         // Create the initial worker.
         worker = new AstroWorker(this);
