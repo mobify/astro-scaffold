602aa42374d6e731ae6e94cc2277126f3fad91e9
Liz Cross <elizabeth@mobify.com>
2015-10-28 14:46:19 -0700

    Updating to swift 2.0, updating astro, added window.run to app.js, added ability to access insecure sites to info plist.

--- app/app.js
+++ app/app.js
@@ -1,53 +1,57 @@
-require([
-    'astro-full',
-    'bluebird',
-    'application',
-    'plugins/navigationPlugin',
-    'plugins/anchoredLayoutPlugin'
-],
-function(
-    Astro,
-    Promise,
-    Application,
-    NavigationPlugin,
-    AnchoredLayoutPlugin
-) {
+window.run = function() {
+    require([
+        'astro-full',
+        'bluebird',
+        'application',
+        'plugins/navigationPlugin',
+        'plugins/anchoredLayoutPlugin'
+    ],
+    function(
+        Astro,
+        Promise,
+        Application,
+        NavigationPlugin,
+        AnchoredLayoutPlugin
+    ) {
 
-    // Enter your site url here
-    var baseUrl = 'http://www.google.com/';
-    var startUriPromise = Application.getStartUri();
+        // Enter your site url here
+        var baseUrl = 'http://www.google.com/';
+        var startUriPromise = Application.getStartUri();
 
-    // Initialize plugins
-    var mainNavigationViewPromise = NavigationPlugin.init();
-    var layoutPromise = AnchoredLayoutPlugin.init();
+        // Initialize plugins
+        var mainNavigationViewPromise = NavigationPlugin.init();
+        var layoutPromise = AnchoredLayoutPlugin.init();
 
-    // Start the app at the base url or provided start uri (deep link launch)
-    Promise.join(mainNavigationViewPromise, startUriPromise, function(mainNavigationView, uri) {
-        if (uri != null) {
-            mainNavigationView.navigate(uri);
-        } else {
-            mainNavigationView.navigate(baseUrl);
-        }
-    });
-
-    // Listen for deep link events once app is running
-    Application.on('receivedDeepLink', function(params) {
-        mainNavigationViewPromise.then(function(mainNavigationView) {
-            var uri = params.uri;
+        // Start the app at the base url or provided start uri (deep link launch)
+        Promise.join(mainNavigationViewPromise, startUriPromise, function(mainNavigationView, uri) {
             if (uri != null) {
                 mainNavigationView.navigate(uri);
+            } else {
+                mainNavigationView.navigate(baseUrl);
             }
         });
-    });
 
-    // Use the mainNavigationView as the main content view for our layout
-    Promise.join(layoutPromise, mainNavigationViewPromise, function(layout, mainNavigationView) {
-        layout.setContentView(mainNavigationView);
-    });
+        // Listen for deep link events once app is running
+        Application.on('receivedDeepLink', function(params) {
+            mainNavigationViewPromise.then(function(mainNavigationView) {
+                var uri = params.uri;
+                if (uri != null) {
+                    mainNavigationView.navigate(uri);
+                }
+            });
+        });
 
-    // Set the main view as the layout
-    layoutPromise.then(function(layout) {
-        Application.setMainViewPlugin(layout);
-    });
+        // Use the mainNavigationView as the main content view for our layout
+        Promise.join(layoutPromise, mainNavigationViewPromise, function(layout, mainNavigationView) {
+            layout.setContentView(mainNavigationView);
+        });
 
-}, undefined, true);
+        // Set the main view as the layout
+        layoutPromise.then(function(layout) {
+            Application.setMainViewPlugin(layout);
+        });
+
+    }, undefined, true);
+};
+// Comment out next line for JS debugging
+window.run();
