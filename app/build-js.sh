c0c2b10f90add9c997ae29be638c33bcf31b8c51
Jeremy Wiebe <jeremy@mobify.com>
2016-01-08 11:20:01 -0800

    Get rid of tab chars

--- app/build-js.sh
+++ app/build-js.sh
@@ -12,12 +12,12 @@ export PATH=$PATH:/usr/local/bin
 # Source additional configuration from `user-env.sh` in Astro's root.
 if [ -f user-env.sh ]; then
     echo "Found user-env.sh"
-	source user-env.sh
+    source user-env.sh
 fi
 
 if ! which npm 1>/dev/null 2>&1; then
-	echo "Cannot find 'npm'.  Please ensure 'npm' is in your PATH."
-	exit 1
+    echo "Cannot find 'npm'.  Please ensure 'npm' is in your PATH."
+    exit 1
 fi
 
 # Build app.js.
