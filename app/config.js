c56df5787a7ee3051edace6984491ed5773a1be7
Shawn Jansepar <shawn@mobify.com>
2016-01-24 18:28:55 -0800

    Grab Astro and Cordova inside node_modules

--- app/config.js
+++ app/config.js
@@ -1,13 +1,13 @@
 require.config({
     baseUrl: '.',
     paths: {
-        'astro-base': '../astro/js/src/astro-base',
-        'astro-full': '../astro/js/src/astro-full',
-        'application': '../astro/js/src/application',
+        'astro-base': '../node_modules/astro-sdk/js/src/astro-base',
+        'astro-full': '../node_modules/astro-sdk/js/src/astro-full',
+        'application': '../node_modules/astro-sdk/js/src/application',
         'bluebird': '../node_modules/bluebird/js/browser/bluebird',
-        'plugins': '../astro/js/src/plugins',
-        'plugin-manager': '../astro/js/src/plugin-manager',
-        'vendor/backbone-events': '../astro/js/vendor/backbone-events',
-        'worker': '../astro/js/src/worker'
+        'plugins': '../node_modules/astro-sdk/js/src/plugins',
+        'plugin-manager': '../node_modules/astro-sdk/js/src/plugin-manager',
+        'vendor/backbone-events': '../node_modules/astro-sdk/js/vendor/backbone-events',
+        'worker': '../node_modules/astro-sdk/js/src/worker'
     }
 });
