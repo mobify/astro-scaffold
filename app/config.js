a89026f25b942c53d568d86dc925344f51f7f38e
Shawn Jansepar <shawn@mobify.com>
2016-01-24 18:10:12 -0800

    Move package.json into root folder

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ require.config({
         'astro-base': '../astro/js/src/astro-base',
         'astro-full': '../astro/js/src/astro-full',
         'application': '../astro/js/src/application',
-        'bluebird': 'node_modules/bluebird/js/browser/bluebird',
+        'bluebird': '../node_modules/bluebird/js/browser/bluebird',
         'plugins': '../astro/js/src/plugins',
         'plugin-manager': '../astro/js/src/plugin-manager',
         'vendor/backbone-events': '../astro/js/vendor/backbone-events',
