724511e3e47041eadc56e4da77845297323db6cb
lilyzh <lzhang@mobify.com>
2016-02-22 15:36:29 -0800

    Fix the linting errors.

--- app/tests/system/pages/web/homepage.js
+++ app/tests/system/pages/web/homepage.js
@@ -1,19 +1,19 @@
 module.exports = {
   setUp: function(browser) {
-    browser
+      browser
       .pause(3000)
       .contexts(function(result) {
-        browser.log(result.value);
+          browser.log(result.value);
       });
   },
 
   'Verify webview URL': function(browser) {
-    browser
+      browser
       .contexts(function(result) {
-        browser
+          browser
           .setContext(result.value[result.value.length - 1])
           .url(function(url) {
-            browser.log(url.value);
+              browser.log(url.value);
           })
           .assert.urlContains('https://webpush-you-host.mobifydemo.io/about/');
       })
