e2006d2f449a182dd2a072ca00b0c9d77b0d05b1
Liz Cross <elizabeth@mobify.com>
2016-03-14 10:58:46 -0700

    Adding warning.

--- app/scaffold-config/baseConfig.js
+++ app/scaffold-config/baseConfig.js
@@ -1,7 +1,9 @@
 /* global AstroNative*/
 define([],
 function() {
+    // Line altered by generator. If changing please change generator as well!!
     var iosUsingTabLayout = false;
+
     var colors = {
         primaryColor: '#007ba7',
         secondaryColor: '#007ba7',
