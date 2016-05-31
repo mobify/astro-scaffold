049f1c64d0e1c46da5c9ae5f1d2e74bba61f72c1
Tony Dinh <tdinh@mobify.me>
2016-05-31 15:00:33 -0700

    🔥 use npm navitron & get plugin.js to resolve as a dep in app-www

--- app/app-www/js/config.js
+++ app/app-www/js/config.js
@@ -3,7 +3,8 @@ require.config({
     paths: {
         '$': 'jquery.min',
         'velocity': 'velocity.min',
-        'navitron': 'navitron.min'
+        'navitron': 'navitron.min',
+        'plugin': 'plugin.min'
     },
     'shim': {
         '$': {
