234572be1ce6e78595cfec76c25b64a2d47e5085
Jason Voll <jason@mobify.com>
2016-06-17 17:16:19 -0700

    cleanup a bit

--- app/app-controllers/navigationController.js
+++ app/app-controllers/navigationController.js
@@ -158,7 +158,7 @@ function(
 
     NavigationController.prototype.navigateMainViewToNewRoot = function(url, title) {
         var self = this;
-        this.navigationView.popToRoot({animated: true})
+        this.popToRoot({animated: true})
             .then(function() {
                 return self.navigationView.getTopPlugin();
             })
