3b7004c6d7d86fff7f05c223dac9612683da0233
Kerr Marin Miller <kmarinmiller@mobify.com>
2016-09-30 12:54:38 -0700

    HYB-868: Remove old build-js and add items to prebuild script

--- scripts/preBuild.sh
+++ scripts/preBuild.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Running pre-build script"
+# Add navitron & its dependencies to the app bundle
+cp $MYPATH/node_modules/grunt-requirejs/node_modules/requirejs/require.js $MYPATH/app/app-www/js/build
+cp $MYPATH/node_modules/navitron/node_modules/plugin/dist/plugin*.js $MYPATH/app/app-www/js/build
+cp $MYPATH/node_modules/navitron/node_modules/velocity-animate/velocity.* $MYPATH/app/app-www/js/build
+cp $MYPATH/node_modules/navitron/dist/navitron*.js $MYPATH/app/app-www/js/build
+
+cp $MYPATH/node_modules/jquery/dist/jquery.min.js $MYPATH/app/app-www/js/build
