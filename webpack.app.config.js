e645a301e61bfe7d9edf08001cbb54364edf4033
Jake Runzer <jakerunzer@gmail.com>
2016-11-23 16:08:16 -0800

    initial build and running with webpack

---
+++ webpack.app.config.js
@@ -0,0 +1,68 @@
+var webpack = require('webpack');
+var path = require('path');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+var rootDir = process.cwd();
+var entry = path.resolve(rootDir, 'app/app.js');
+var outDir = path.resolve(rootDir, 'build');
+
+var isProd = process.env.NODE_ENV === 'production';
+var analyzeBundle = process.env.NODE_ENV === 'Analyze';
+
+var config = {
+    entry: entry,
+    output: {
+        filename: 'app.js',
+        path: outDir
+    },
+    resolve: {
+        modules: [
+            path.resolve(rootDir),
+            path.resolve(rootDir, 'vendor/astro/src'),
+            path.resolve(rootDir, 'vendor/astro/src/global'),
+            path.resolve(rootDir, 'vendor/astro'),
+            'node_modules'
+        ],
+        alias: {
+            astro: path.resolve(rootDir, 'node_modules/astro-sdk/js/src/'),
+            vendor: path.resolve(rootDir, 'node_modules/astro-sdk/js/vendor/'),
+            bluebird: path.resolve(rootDir, 'node_modules/astro-sdk/node_modules/bluebird')
+        }
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
+        }),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                eslint: {
+                    configFile: path.resolve(rootDir, '.eslintrc.yml')
+                }
+            }
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            enforce: 'pre',
+            use: ['eslint-loader'],
+            exclude: /node_modules/
+        }]
+    }
+};
+
+if (isProd) {
+    config.plugins = config.plugins.concat([
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]);
+} else if (analyzeBundle) {
+    config.plugins = config.plugins.concat([
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: true
+        })
+    ]);
+}
+
+module.exports = config;
